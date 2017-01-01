3c925bbb6bcd40c1b673ba167d89f6ba50e59fca
Utku Gultopu <ugultopu@gmail.com>
2017-01-01 22:11:05 +0300

    Add login view

--- FriendTracker/urls.py
+++ FriendTracker/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from FriendTrackerApp.views import login
 
 urlpatterns = [
+    url(r'^login/$', login),
     url(r'^admin/', admin.site.urls),
 ]
--- FriendTrackerApp/views.py
+++ FriendTrackerApp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+
+@csrf_exempt
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponse(json.dumps({'status': 0,
+                                        'sessionid': request.session.session_key}))
+    return HttpResponse(json.dumps({'status': 1}))
+
