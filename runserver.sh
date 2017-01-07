56a5ddede2e4627f9cad72d88f67e27aaa975571
Utku Gultopu <ugultopu@gmail.com>
2017-01-07 18:34:57 +0300

    Fix runserver.sh so that ^C will terminate the stunnel process

--- runserver.sh
+++ runserver.sh
@@ -1,8 +1,7 @@
-stunnel stunnel/dev_https &
-python manage.py runserver
-
 kill_stunnel() {
 	kill $stunnel_pid
 }
-
-trap kill_stunnel exit stunnel stunnel/dev_https & stunnel_pid=$1
+trap kill_stunnel exit
+stunnel stunnel/dev_https &
+stunnel_pid=$!
+python manage.py runserver
