f6e66c12537c4c2d90c550d9ec1a62a565b1b393
Utku Gultopu <ugultopu@gmail.com>
2017-01-29 14:16:47 +0300

    Remove HTTP session as well upon disconnection

--- FriendTrackerApp/consumers.py
+++ FriendTrackerApp/consumers.py
@@ -1,6 +1,7 @@
 from channels.auth import channel_session_user_from_http, channel_session_user
 from channels import Group
 from FriendTrackerApp.models import Location, OnlineUser, Follower
+from django.contrib.sessions.models import Session
 from .detlogging import detlog
 import json
 
@@ -30,6 +31,18 @@ def location_send(message):
 @channel_session_user
 def location_disconnect(message):
     message.channel_session.flush()
+    # FIXME With a new commit, the line below is no longer needed to ensure
+    # session deletion. Remove it after testing that session gets deleted upon
+    # removing the line.
     message.channel_session.modified = False
+    user_id = message.channel_session.get('__auth_user_id')
+    delete_session(user_id)
     Group('online-users').discard(message.reply_channel)
+
+
+def delete_session(user_id):
+    for session in Session.objects.all():
+        if session.get_decoded().get('__auth_user_id') == user_id:
+            session.delete()
+
 
