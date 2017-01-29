a788e0efa93e3678771dd3841e42ac1f2f6ae799
Utku Gultopu <ugultopu@gmail.com>
2017-01-29 23:31:17 +0300

    Add missing import

--- FriendTrackerApp/models.py
+++ FriendTrackerApp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.sessions.base_session import AbstractBaseSession
 from django.db import models
+from FriendTrackerApp.sessions import SessionStore
 
 
 class Location(models.Model):
